acf01b546f323714f987a3038be8fc06c57bea53
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-07-02 09:54:01 -0300

    initial config redux

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,8 @@
+export function AddTodo() {
+  return (
+    <form>
+      <input type="text" placeholder="Novo to-do" />
+      <button type="submit">Adicionar</button>
+    </form>
+  );
+}
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,12 @@
+import { useSelector } from "react-redux";
+
+export function TodoList() {
+  const todos = useSelector((store) => store.todo);
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </ul>
+  );
+}
