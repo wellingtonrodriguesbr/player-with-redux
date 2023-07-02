fb0fd1cab3503457d2123f29018bf2ec390d0ff5
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-07-02 10:15:54 -0300

    feat: dispatch actions

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,7 +1,26 @@
+import { useState, FormEvent } from "react";
+import { useDispatch } from "react-redux";
+import { add } from "../store";
+
 export function AddTodo() {
+  const [newTodo, setNewTodo] = useState("");
+  const dispatch = useDispatch();
+
+  function handleAddNewTodo(e: FormEvent) {
+    e.preventDefault();
+
+    dispatch(add({ newTodo }));
+    setNewTodo("");
+  }
+
   return (
-    <form>
-      <input type="text" placeholder="Novo to-do" />
+    <form onSubmit={handleAddNewTodo}>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Novo to-do"
+      />
       <button type="submit">Adicionar</button>
     </form>
   );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,23 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useSelector } from "react-redux";
 
 const todoSlice = createSlice({
-  name: 'todo',
-  initialState: ['Estudar Redux'],
-  reducers: {}
-})
+  name: "todo",
+  initialState: ["Estudar Redux"],
+  reducers: {
+    add: (state, action) => {
+      state.push(action.payload.newTodo);
+    },
+  },
+});
 
 export const store = configureStore({
   reducer: {
-    todo: todoSlice.reducer
-  }
-})
+    todo: todoSlice.reducer,
+  },
+});
+
+export const { add } = todoSlice.actions;
+
+export type RootState = ReturnType<typeof store.getState>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
