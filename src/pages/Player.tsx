a57d89c17aa622133e0a8f385fd2bbdac04dccf0
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-07-22 10:49:18 -0300

    feat: async thunks

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,28 +3,24 @@ import { MessageCircle } from "lucide-react";
 import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
-import { useAppSelector } from "../store";
-import { start, useCurrentLesson } from "../store/slices/player";
+import { useAppDispatch, useAppSelector } from "../store";
+import { loadCourse, useCurrentLesson } from "../store/slices/player";
 import { useEffect } from "react";
-import { api } from "../lib/axios";
-import { useDispatch } from "react-redux";
 
 export function Player() {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const modules = useAppSelector((state) => state.player.course?.modules);
   const { currentLesson } = useCurrentLesson();
-
-  useEffect(() => {
-    api.get("/courses/1").then((response) => {
-      dispatch(start(response.data));
-    });
-  }, []);
 
   useEffect(() => {
     if (currentLesson) {
       document.title = `Assistindo: ${currentLesson?.title}`;
     }
   }, [currentLesson]);
+
+  useEffect(() => {
+    dispatch(loadCourse());
+  }, []);
 
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
